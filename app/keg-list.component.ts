07886432c3e05cdf8f6278883e0b8b307e8587c5
Angie Smith and chris martinez <angie@>
2016-10-19 14:05:00 -0700

    add danger class and elements when pinks below serving number

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -5,14 +5,15 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'keg-list',
   template: `
-  <h3>Filter By Servings:</h3>
+  <h3 >Filter By Servings:</h3>
   <input type=number (change)="filterServings($event.target.value)">
   <div *ngFor="let currentKeg of childKegList | servings:selectedServings">
     <h1>{{currentKeg.name}}</h1>
+    <h1 class="danger" *ngIf="currentKeg.servings < 10">Order More!!</h1>
     <p>by {{currentKeg.brand}}</p>
     <p>Alcohol Content: {{currentKeg.alcoholContent}} % </p>
-    <p>$ {{currentKeg.price}}</p>
-    <p>Pints left: {{currentKeg.servings}}</p>
+    <p [ngClass]=getPriceClass(currentKeg.price) > $ {{currentKeg.price}}</p>
+    <p [ngClass]=servingWarning(currentKeg.servings)>Pints left: {{currentKeg.servings}}</p>
     <label>Sold Pints:</label>
     <input #pintsSold type="number" value=1>
     <button (click)="editButtonClicked(currentKeg)">Edit</button>
@@ -25,6 +26,7 @@ export class KegListComponent {
   @Input() childKegList: Keg[];
   @Output() clickSender = new EventEmitter();
   public selectedServings: number = 124;
+
   editButtonClicked(kegToEdit: Keg){
     this.clickSender.emit(kegToEdit);
   }
@@ -34,4 +36,23 @@ export class KegListComponent {
   filterServings(servingInput) {
     this.selectedServings = servingInput;
   }
+  getPriceClass(price){
+    if(price < 10){
+      return "cheap";
+    } else {
+      return "moderate";
+    }
+  }
+  servingWarning(servings) {
+    if(servings <10) {
+      return "danger";
+    }
+
+    // else if (servings < 10) {
+    //   return "danger";
+    // } else {
+    //   return "save";
+    // }
+  }
+
 }
